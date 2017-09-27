2cfcecc00235d8019922bc6e169864c137baabed
Vijay <vijaykumarsaurav@gmail.com>
2017-09-27 11:22:45 +0530

    V1 has updated

---
+++ controller/refresh_main_dashboard.php
@@ -0,0 +1,37 @@
+<?php
+/**
+*  Model for Generate Subtraction
+*
+* @package    Model
+* @author     Vijay Kumar
+* 
+*
+*/
+
+   require_once('../model/DriverModel.php');
+
+   require_once('../model/database.php');
+
+   // $noq = $_REQUEST['noq'];
+
+
+   $db =  new Database;
+   $obj = new DriverModel( $db );
+
+   $json_data = array();
+
+   
+   $json_data = $obj->refreshMainDashborad();
+  
+
+ 
+/*
+echo "<pre>";
+echo $_SERVER['REQUEST_URI'];
+echo "</pre>";*/
+
+header('Content-Type: application/json');
+echo json_encode($json_data);
+
+   
+?>
